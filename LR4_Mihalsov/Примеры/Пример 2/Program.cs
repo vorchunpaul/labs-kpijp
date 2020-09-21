60c8af796efd2f8f67f87edb51477f17ab4993cc
vorchun <vorchun@tuta.io>
2020-09-21 13:22:31 +0300

    add complite tast for LR4

---
+++ LR4_Mihalsov/Примеры/Пример 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Prim_Metod2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a = 3, b = 4, c = 5;
+            Console.WriteLine("Длины сторон:\n a={0} b={1} c={2}", a, b, c); if (Treug(a, b, c))
+                Console.WriteLine("Да, треугольник существует");
+            else
+                Console.WriteLine("Нет, треугольник не существует");
+
+            Console.ReadKey(true);
+        }
+        public static bool Treug(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a) return true; else return false;
+        }
+    }
+}
---
+++ LR4_Mihalsov/Примеры/Пример 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Prim_Metod5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int s; s = fSum(4, 3, 12);
+            Console.WriteLine("Сумма = " + s);
+
+            Console.ReadKey(true);
+        }
+        public static int fSum(params int[] x)
+        {
+            int s = 0; for (int i = 0; i < x.Length; i++) s += x[i]; return s;
+        }
+    }
+}
