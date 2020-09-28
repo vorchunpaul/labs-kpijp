e4f5d40a0a45358db1b99ebff992dd09acf01782
vorchun <vorchun@tuta.io>
2020-09-28 20:17:41 +0300

    add p8

---
+++ LR8_Mihalsov/Примеры/Пример 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Example
+{
+    static void Main()
+    {
+        // Допустим в исходной строке нужно найти все числа,
+        // соответствующие стоимости продукта
+        string input = "Добро пожаловать в наш магазин, вот наши цены: " +
+        "1 кг. яблок - 20 руб. " +
+        "2 кг. апельсинов - 30 руб. " +
+        "0.5 кг. орехов - 50 руб.";
+        string pattern = @"\b(\d+\W?руб)";
+        Regex regex = new Regex(pattern);
+        // Достигаем того же результата что и в предыдущем примере,
+        // используя метод Regex.Matches() возвращающий MatchCollection
+        foreach (Match match in regex.Matches(input))
+        {
+            Console.WriteLine(match.Groups[1].Value);
+        }
+
+        Console.ReadKey(true);
+    }
+}
---
+++ LR8_Mihalsov/Примеры/Пример 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Example
+{
+    static void Main()
+    {
+        // Допустим в исходной строке нужно заменить "руб." на "$",
+        // а стоимость переместить после знака $
+        string input = "Добро пожаловать в наш магазин, вот наши цены:\n" +
+                    "\t 1 кг. яблок - 20 руб. \n" +
+                    "\t 2 кг. апельсинов - 30 руб. \n" +
+                    "\t 0.5 кг. орехов - 50 руб. \n";
+        Console.WriteLine("Исходная строка:\n {0}", input);
+        // В шаблоне используются 2 группы
+        string pattern = @"\b(\d+)\W?(руб.)";
+        // Строка замены "руб." на "$"
+        string replacement1 = "$$$1"; // Перед первой группой ставится знак $,
+                                      // вторая группа удаляется без замены
+        input = Regex.Replace(input, pattern, replacement1);
+        Console.WriteLine("\nВидоизмененная строка: \n" + input);
+
+        Console.ReadKey(true);
+    }
+}
