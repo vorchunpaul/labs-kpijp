a47b8f90eb33083721b3ac899cc0d654828c848f
vorchunpaul <vorchun@tuta.io>
2020-10-27 21:22:26 +0300

    add m9 m10 p9

---
+++ LR9_Mihalsov/Примеры/Пример 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
+{
+    public struct Book
+    {
+        // members
+        public string Name;
+        public int Price;
+        // methods
+        public void Show()
+        {
+            Console.WriteLine("Название книги {0}, Цена {1}", Name, Price);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем структуру без оператора new
+            Book book;
+            // инициализируем данные класса
+            book.Name = "Волшебник Земноморья";
+            book.Price = 70;
+            // Показываем результат на экране
+            book.Show();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LR9_Mihalsov/Примеры/Пример 5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
+{
+    public struct Book
+    {
+        // members
+        public string Name;
+        public int Price;
+        // constructor
+        public Book(String name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+        // methods
+        public void Show()
+        {
+            Console.WriteLine("Название книги {0}, Цена {1}", Name, Price);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаем объект структуры с оператором new с параметрами
+            Book book = new Book("Волшебник Земноморья", 90);
+            // наш конструктор установит значения
+            // Name = "Волшебник Земноморья"
+            // Price = 90
+            // Показываем результат на экране
+            book.Show();
+            Console.ReadKey();
+        }
+    }
+}
