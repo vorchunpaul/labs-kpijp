0a97337ee6cd363b36b95880117ca3bb1c6bc529
vorchunpaul <vorchun@tuta.io>
2020-11-18 09:56:43 +0300

    add z14 p14

---
+++ LR14_Mihalsov/Примеры/Пример 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пример_1
+{
+    class Program
+    {
+        class Counter
+        {
+            public event EventHandler ThresholdReached;
+            public virtual void OnThresholdReached(EventArgs e)
+            {
+                EventHandler handler = ThresholdReached; 
+                handler?.Invoke(this, e);
+            }
+        }
+        public class ThresholdReachedEventArgs : EventArgs 
+        { 
+            public int Threshold { get; set; } 
+            public DateTime TimeReached { get; set; } 
+        }
+        static void c_ThresholdReached(object sender, EventArgs e)
+        {
+            Console.WriteLine("The threshold was reached.");
+        }
+        static void Main()
+        {
+            var c = new Counter();
+            c.ThresholdReached += c_ThresholdReached;
+            c.OnThresholdReached(new ThresholdReachedEventArgs());
+            Console.ReadKey(true);
+        }
+        
+    }
+}
